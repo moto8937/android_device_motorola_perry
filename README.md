594ff07c4e4d05c282280dab8f9f0bfdece6688e
Sultan Qasim Khan <sultanqasim@gmail.com>
2017-10-08 01:40:23 -0400

    perry: Initial skeleton tree
    Everything is in common
    
    Change-Id: I8e333f65ab5fedea619c38a6c2234b519b0b7443

---
+++ README.md
@@ -0,0 +1,22 @@
+Unified Device Tree for Moto E4 Qualcomm (perry)
+================================================
+
+The Motorola Moto E4 (codenamed _"perry"_) is a low-range smartphone from
+Motorola Mobility. It was announced in June 2017.
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Quad-core 1.4 GHz Cortex-A53
+Chipset | Qualcomm MSM8917 Snapdragon 425
+GPU     | Adreno 308
+Memory  | 2 GB RAM
+Shipped Android Version | 7.1.1
+Storage | 16 GB
+MicroSD | Up to 32 GB
+Battery | Li-Ion 2800mAh battery
+Display | 720 x 1280 pixels, 5.0 inches (~294 ppi pixel density)
+Camera  | 8MP, autofocus, flash LED
+
+Copyright 2017 - The LineageOS Project.
+
+![Moto E4](https://www.motorola.com/sites/default/files/library/storage/products/smartphones/moto-e4-NA-1000.png "Moto E4")
---
+++ lineage.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/motorola/perry/full_perry.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := perry
+PRODUCT_NAME := lineage_perry
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=motorola/perry/perry:7.1.1/NPNS26.118-22-1/1:user/release-keys \
+    PRIVATE_BUILD_DESC="perry-user 7.1.1 NPNS26.118-22-1 1 release-keys" \
+    PRODUCT_NAME="Moto E4"
